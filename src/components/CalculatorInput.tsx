1ff53cf8a124a4261219b49e82964ae873424a10
LuanVM1810 <Vomongluan@gmail.com>
2024-05-14 15:31:40 +0700

    push to merge

--- src/components/CalculatorInput.tsx
+++ src/components/CalculatorInput.tsx
@@ -1,19 +1,7 @@
 import { Button, Card, Typography } from "@mui/material";
-import PublicOutlinedIcon from "@mui/icons-material/PublicOutlined";
-import ScienceOutlinedIcon from "@mui/icons-material/ScienceOutlined";
-import { useState } from "react";
+import { originDiamondType, shapeDiamond } from "../pages/CalculatePage";
 
 const CalculatorInput = () => {
-  const [origin, setOrigin] = useState<boolean>(true);
-
-  const clickOrigin1 = () => {
-    setOrigin(true);
-  };
-
-  const clickOrigin2 = () => {
-    setOrigin(false);
-  };
-
   return (
     <div>
       <Typography
@@ -33,45 +21,60 @@ const CalculatorInput = () => {
             padding: 10,
           }}
         >
-          <Button
-            variant="contained"
-            size="small"
-            style={{
-              width: "198px",
-              backgroundColor: origin ? "#4F46E5" : "#F3F4F6",
-            }}
-            onClick={clickOrigin1}
-          >
-            <PublicOutlinedIcon
-              style={{ height: "20px", color: origin ? "white" : "#111827" }}
-            />
-            <Typography
-              color={origin ? "white" : "#111827"}
-              textTransform={"none"}
-            >
-              Natural
-            </Typography>
-          </Button>
+          <ul style={{ listStyle: "none", display: "flex" }}>
+            {originDiamond.map((originMap, index) => {
+              return (
+                <li style={{ marginRight: "10px" }}>
+                  <Button
+                    variant="contained"
+                    size="small"
+                    key={originMap.id}
+                    style={{
+                      width: "198px",
+                      backgroundColor: "#F3F4F6",
+                    }}
+                    onClick={() => handleOriginClick(originMap, index)}
+                  >
+                    {originMap.icon}
+                    <Typography
+                      color={"#111827"}
+                      textTransform={"none"}
+                      fontSize={"12px"}
+                    >
+                      {originMap.name}
+                    </Typography>
+                  </Button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
 
-          <Button
-            variant="contained"
-            size="small"
-            style={{
-              width: "198px",
-              backgroundColor: origin ? "#F3F4F6" : "#4F46E5",
-            }}
-            onClick={clickOrigin2}
-          >
-            <ScienceOutlinedIcon
-              style={{ height: "20px", color: origin ? "#111827" : "white" }}
-            />
-            <Typography
-              color={origin ? "#111827" : "white"}
-              textTransform={"none"}
-            >
-              Lab Grown
-            </Typography>
-          </Button>
+        <Typography style={{ color: "#6B7280" }}>SHAPE</Typography>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            padding: 10,
+          }}
+        >
+          {shapeArray.map((shapeArr) => {
+            return (
+              <Button
+                variant="contained"
+                size="small"
+                key={shapeArr.id}
+                style={{
+                  width: "72px",
+                  backgroundColor: "#4F46E5",
+                }}
+              >
+                <Typography color={"white"} textTransform={"none"}>
+                  {shapeArr.name}
+                </Typography>
+              </Button>
+            );
+          })}
         </div>
       </Card>
     </div>
--- src/pages/CalculatePage.tsx
+++ src/pages/CalculatePage.tsx
@@ -1,6 +1,16 @@
-import React from "react";
-import CalculatorInput from "../components/CalculatorInput";
 import { Container } from "@mui/material";
+import CalculatorInput from "../components/CalculatorInput";
+
+export type shapeDiamond = {
+  id: number;
+  name: string;
+};
+
+export type originDiamondType = {
+  id: number;
+  name: string;
+  icon: JSX.Element;
+};
 
 const CalculatePage = () => {
   return (
