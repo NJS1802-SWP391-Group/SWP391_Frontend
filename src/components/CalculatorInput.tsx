22200ccb863c5d06eb3b96a9dd4cbfa68e1bf5cf
LuanVM1810 <Vomongluan@gmail.com>
2024-05-14 15:37:13 +0700

    push to merge

--- src/components/CalculatorInput.tsx
+++ src/components/CalculatorInput.tsx
@@ -1,5 +1,4 @@
-import { Button, Card, Typography } from "@mui/material";
-import { originDiamondType, shapeDiamond } from "../pages/CalculatePage";
+import { Card, Typography } from "@mui/material";
 
 const CalculatorInput = () => {
   return (
@@ -20,62 +19,7 @@ const CalculatorInput = () => {
             justifyContent: "space-around",
             padding: 10,
           }}
-        >
-          <ul style={{ listStyle: "none", display: "flex" }}>
-            {originDiamond.map((originMap, index) => {
-              return (
-                <li style={{ marginRight: "10px" }}>
-                  <Button
-                    variant="contained"
-                    size="small"
-                    key={originMap.id}
-                    style={{
-                      width: "198px",
-                      backgroundColor: "#F3F4F6",
-                    }}
-                    onClick={() => handleOriginClick(originMap, index)}
-                  >
-                    {originMap.icon}
-                    <Typography
-                      color={"#111827"}
-                      textTransform={"none"}
-                      fontSize={"12px"}
-                    >
-                      {originMap.name}
-                    </Typography>
-                  </Button>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        <Typography style={{ color: "#6B7280" }}>SHAPE</Typography>
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            padding: 10,
-          }}
-        >
-          {shapeArray.map((shapeArr) => {
-            return (
-              <Button
-                variant="contained"
-                size="small"
-                key={shapeArr.id}
-                style={{
-                  width: "72px",
-                  backgroundColor: "#4F46E5",
-                }}
-              >
-                <Typography color={"white"} textTransform={"none"}>
-                  {shapeArr.name}
-                </Typography>
-              </Button>
-            );
-          })}
-        </div>
+        ></div>
       </Card>
     </div>
   );
