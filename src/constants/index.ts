1849c9afd3f97e19c6cd3e7b04fbcefde16ffdf1
LuanVM1810 <Vomongluan@gmail.com>
2024-06-05 22:19:34 +0700

    routing by roleName

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,3 +5,4 @@ export const ROLE_ID_KEY = "roleId";
 export const USERNAME = "username";
 export const USER_TOKEN_KEY = "token";
 export const USER_FULLNAME_KEY = "fullName";
+export const LOGIN_SUCCESS = "Login Successfully";
