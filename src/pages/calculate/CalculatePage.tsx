7fe1288cec7cded5c232d17fdcc028be6f3a1268
hungduongdc <hung.duong@deltacognition.com>
2024-06-06 02:29:44 +0700

    UI of calculator page

--- src/pages/calculate/CalculatePage.tsx
+++ src/pages/calculate/CalculatePage.tsx
@@ -1,9 +1,10 @@
 import { Container } from "@mui/material";
 import React from "react";
-import CalculatorInput from "../../components/CalculatorInput";
 import Footer from "../../components/footer/Footer";
 import Navbar from "../../components/navbar/Navbar";
-
+import CalculatorInput from "../../components/calculator/CalculatorInput";
+import CalculatorOutput from "../../components/calculator/CalculatorOutput";
+import "./CalculatePage.css";
 export type shapeDiamond = {
   id: number;
   name: string;
@@ -20,7 +21,10 @@ const CalculatePage = () => {
     <React.Fragment>
       <Navbar />
       <Container>
-        <CalculatorInput />
+        <div className="cal">
+          <CalculatorInput />
+          <CalculatorOutput />
+        </div>
       </Container>
       <Footer />
     </React.Fragment>
