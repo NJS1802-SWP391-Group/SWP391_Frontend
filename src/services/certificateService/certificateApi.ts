f81fbee0ba0162aa0e76ea84deae739a1de019ef
Huy-techSw <lequanghuy30012003@gmail.com>
2024-06-12 22:48:48 +0700

    Done Api changeStatusCertificated

--- src/services/certificateService/certificateApi.ts
+++ src/services/certificateService/certificateApi.ts
@@ -4,6 +4,10 @@ const certificateApi = {
   getCertificateByID: (resultId: number) => {
     return axiosClient.get(`/Result/Get-Result-By-Id/${resultId}`);
   },
+
+  changeStatusToCertificated: (orderDetailId: number) => {
+    return axiosClient.put(`/OrderDetail/Approve-Valuate/${orderDetailId}`);
+  },
 };
 
 export default certificateApi;
