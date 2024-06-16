d916b766ebd2a68171b0ff3ff40ed1c8dfee3e00
Huy-techSw <lequanghuy30012003@gmail.com>
2024-06-16 17:34:25 +0700

    Crud for service and serviceDetail

--- src/components/manager/serviceDetail.tsx
+++ src/components/manager/serviceDetail.tsx
@@ -1,6 +1,11 @@
 import {
   Box,
   Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
   Paper,
   Table,
   TableBody,
@@ -9,13 +14,14 @@ import {
   TableHead,
   TablePagination,
   TableRow,
+  TextField,
+  Typography,
   styled,
 } from "@mui/material";
 import { useEffect, useState } from "react";
-
+import PlusAdd from "../../assets/PlusAdd.png";
 import { ServiceDetailResponse } from "../../interfaces/serviceDetail/ServiceDetail";
 import serviceDetailApi from "../../services/serviceDetailApi";
-
 const ServiceDetail = () => {
   const [serviceDetailList, setServiceDetailList] = useState<
     ServiceDetailResponse[]
@@ -85,6 +91,49 @@ const ServiceDetail = () => {
     );
   };
 
+  const handleSaveServiceDetail = () => {
+    if (editServiceDetail) {
+      setServiceDetailList((prevServiceDetail) =>
+        prevServiceDetail.map((m) =>
+          m.serviceDetailID === editServiceDetail.serviceDetailID
+            ? editServiceDetail
+            : m
+        )
+      );
+      setEditServiceDetail(null);
+    } else if (newServiceDetail) {
+      setServiceDetailList((prevServiceDetail) => [
+        ...prevServiceDetail,
+        {
+          ...newServiceDetail,
+          FeID: `FE${prevServiceDetail.length + 1}`,
+          CreatedTime: new Date().toISOString(),
+        },
+      ]);
+      setNewServiceDetail(null);
+    }
+  };
+
+  const handleInputChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = event.target;
+    if (editServiceDetail) {
+      setEditServiceDetail((prevServiceDetail) =>
+        prevServiceDetail ? { ...prevServiceDetail, [name]: value } : null
+      );
+    } else if (newServiceDetail) {
+      setNewServiceDetail((prevServiceDetail) =>
+        prevServiceDetail ? { ...prevServiceDetail, [name]: value } : null
+      );
+    }
+  };
+
+  const handleCloseDialog = () => {
+    setEditServiceDetail(null);
+    setNewServiceDetail(null);
+  };
+
   const handleChangePage = (event: unknown, newPage: number) => {
     setPage(newPage);
   };
@@ -112,15 +161,18 @@ const ServiceDetail = () => {
       >
         <Button
           variant="contained"
-          sx={{ marginLeft: 130 }}
+          sx={{ marginLeft: 127 }}
           onClick={handleAddServiceDetail}
         >
-          New service detail
+          <img src={PlusAdd} height={20} width={20} />
+          <Typography sx={{ paddingLeft: "4px" }}>
+            New service detail
+          </Typography>
         </Button>
       </Box>
       <TableContainer
         component={Paper}
-        sx={{ maxHeight: "50vh", marginTop: "35px" }}
+        sx={{ maxHeight: "50vh", marginTop: "25px" }}
       >
         <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
@@ -195,6 +247,152 @@ const ServiceDetail = () => {
         onPageChange={handleChangePage}
         onRowsPerPageChange={handleChangeRowsPerPage}
       />
+
+      {editServiceDetail && (
+        <Dialog open={true} onClose={handleCloseDialog}>
+          <DialogTitle>Edit ServiceDetail</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Update the details of the ServiceDetail.
+            </DialogContentText>
+            <TextField
+              margin="dense"
+              label="ServiceDetail Code"
+              type="text"
+              fullWidth
+              name="ServiceDetailID"
+              value={editServiceDetail.code}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Min Range"
+              type="text"
+              fullWidth
+              name="MinRange"
+              value={editServiceDetail.minRange}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Max Range"
+              type="text"
+              fullWidth
+              name="MaxRange"
+              value={editServiceDetail.maxRange}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Price"
+              type="text"
+              fullWidth
+              name="Price"
+              value={editServiceDetail.price}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="ExtraPricePerMM"
+              type="text"
+              fullWidth
+              name="ExtraPricePerMM"
+              value={editServiceDetail.extraPricePerMM}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Status"
+              type="text"
+              fullWidth
+              name="Status"
+              value={editServiceDetail.status}
+              onChange={handleInputChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog} color="secondary">
+              Cancel
+            </Button>
+            <Button onClick={handleSaveServiceDetail} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
+
+      {newServiceDetail && (
+        <Dialog open={true} onClose={handleCloseDialog}>
+          <DialogTitle>New ServiceDetail</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Enter the details of the new ServiceDetail.
+            </DialogContentText>
+            <TextField
+              margin="dense"
+              label="ServiceDetail Code"
+              type="text"
+              fullWidth
+              name="ServiceDetailCode"
+              value={newServiceDetail.code}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Price"
+              type="text"
+              fullWidth
+              name="Price"
+              value={newServiceDetail.price}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Min Range"
+              type="text"
+              fullWidth
+              name="MinRange"
+              value={newServiceDetail.minRange}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Max Range"
+              type="text"
+              fullWidth
+              name="MaxRange"
+              value={newServiceDetail.maxRange}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Extra PricePerMM"
+              type="text"
+              fullWidth
+              name="ExtraPricePerMM"
+              value={newServiceDetail.extraPricePerMM}
+              onChange={handleInputChange}
+            />
+            <TextField
+              margin="dense"
+              label="Status"
+              type="text"
+              fullWidth
+              name="Status"
+              value={newServiceDetail.status}
+              onChange={handleInputChange}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseDialog} color="secondary">
+              Cancel
+            </Button>
+            <Button onClick={handleSaveServiceDetail} color="primary">
+              Save
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </Box>
   );
 };
