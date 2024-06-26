e1ba3705033fa5d6c67639e460279485250e7b36
Huy-techSw <lequanghuy30012003@gmail.com>
2024-06-26 15:40:03 +0700

    Create Service

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,9 +1,15 @@
+import { ServiceCreate } from "../interfaces/services/Service";
 import axiosClient from "./axiosClient";
 
 const serviceApi = {
   getAll() {
     const url = "/Service/Get-All";
     return axiosClient.get(url);
+  },
+
+  createService(data: ServiceCreate) {
+    const url = "/Service/Create-Service";
+    return axiosClient.post(url, data);
   },
 };
 
